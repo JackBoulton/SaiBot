c240e3dcdaa8604ab05b24d617dd3c5612bb0192
Jack Boulton <b021333f@student.staffs.ac.uk>
2018-03-27 00:24:25 +0100

    error forwarding and clear to test bot
    All errors now DM'd to me on Discord allowing me to keep track of issues
    in servers I'm not part of.
    Clear function added to test channel

--- SaiKitten.py
+++ SaiKitten.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Server
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
@@ -6,17 +7,42 @@ import requests
 import sys
 from pathlib import Path
 from anime import anime
-
+#read key from file
 key = open('key.txt').readline().rstrip('\n')
+#declare command prefix
 bot = commands.Bot(command_prefix='$$')
 
+#remove default help command
+bot.remove_command('help')
+
+#when bot initialised, change game and print bot details to screen
 @bot.event
 async def on_ready():
-    await bot.change_presence(game = discord.Game(name='with catnip'))
+    await bot.change_presence(game = discord.Game(name='with Mom'))
+    global me
+    me = await bot.get_user_info('155421790963892224')
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('-------')
+
+#
+# --COMMANDS--
+#If message starts with command prefix, read command word and reply
+#
+
+@bot.command(pass_context = True)
+async def clear(ctx):
+    if ctx.message.author == me: await bot.purge_from(Server.get_channel(ctx.message.server,'404013550802042881'))
+    else: await bot.say("You are not permitted to do that")
+
+@bot.command(pass_context = True)
+async def help(ctx):
+    await bot.say("```Commands:\n\t$$a <anime name> - Get information on anime\n\t$$boost - boost\n\t$$BOOST - BOOST\n\t$$help - How did you get here?\n\t$$trap - <:jack:403983194996736031>```")
+
+@bot.command(pass_context = True)
+async def boost(ctx):
+    await bot.say("https://b.catgirlsare.sexy/QwAa.jpg")
 
 @bot.command(pass_context = True)
 async def BOOST(ctx):
@@ -26,6 +52,25 @@ async def BOOST(ctx):
 async def trap(cxt):
     await bot.say("<:jack:403983194996736031>")
 
+#Split message by new line, pop first element in list, split again by spaces.
+#Delete command from message and join elements with a space between each.
+#Call anime function and send info to discord.
+@bot.command(pass_context = True)
+async def a(ctx):
+    split_message = ctx.message.content.split('\n').pop(0).split(' ')
+    del split_message[0]
+    split_message = ' '.join(split_message)
+    #if fail DM error to me
+    try:
+        embed = anime(split_message)
+        await bot.say(embed=embed)
+    except Exception as err:
+        await bot.send_message(me,err)
+
+#
+# --COMMANDS END--
+#
+
 @bot.event
 async def on_message(message):
     if message.author.id != bot.user.id:
@@ -33,18 +78,4 @@ async def on_message(message):
                 await bot.send_message(message.channel, 'Work in progress')
     await bot.process_commands(message)
 
-@bot.command(pass_context = True)
-async def boost(ctx):
-    await bot.say("https://b.catgirlsare.sexy/QwAa.jpg")
-
-
-@bot.command(pass_context = True)
-async def a(ctx):
-    split_message = ctx.message.content.split('\n').pop(0).split(' ')
-    #split_message = ctx.message.content.split(' ')
-    del split_message[0]
-    split_message = ' '.join(split_message)
-
-    embed = anime(split_message)
-    await bot.say(embed=embed)
 bot.run(key)
--- anime.py
+++ anime.py
@@ -1,8 +1,12 @@
 import discord
 import requests
+#store api url
 anilist = 'https://graphql.anilist.co'
+
+
 def anime(split_message):
     #file = Path("anime" + split_message + ".txt")
+    #structure api query
     query = '''
     query($title: String){
         Media (search: $title, type : ANIME, sort : SEARCH_MATCH){
@@ -20,26 +24,26 @@ def anime(split_message):
         }
     }
     '''
-
+    #store variables for query
     variables = {
         'title' : split_message
     }
-
+    #send request to api. Store headers and response
     r = requests.post(anilist, json={'query':query, 'variables': variables})
     headers = r.headers
     response = r.json()
 
-    print(headers)
-
+    #store english and native names. Check if values are empty. If values are
+    #empty, replace with '-'
     english = response['data']['Media']['title']['english']
     native = response['data']['Media']['title']['native']
-
     if not response['data']['Media']['title']['english'] : english = '-'
-
     if not response['data']['Media']['title']['native'] : native = '-'
 
+    #remove <br> from desc
     desc = response['data']['Media']['description'].replace('<br>','')
 
+    #structure and return embed response.
     embed=discord.Embed(title=response['data']['Media']['title']['romaji'], description=desc, color=0x02A9FF)
     embed.set_thumbnail(url=response['data']['Media']['coverImage']['medium'])
     embed.add_field(name="Romaji", value=response['data']['Media']['title']['romaji'], inline=True)
